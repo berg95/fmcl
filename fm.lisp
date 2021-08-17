476f4395c013003a1f7912465bd75ea5629a1b3f
berg95 <52430539+berg95@users.noreply.github.com>
2021-08-16 22:59:05 -0500

    update fm.lisp
    Added support for viewing team stats via a table view

--- fm.lisp
+++ fm.lisp
@@ -30,3 +30,24 @@
   (setf 1score (random 5))
   (setf 2score (random 5))
   (format t "~S ~d - ~d ~S" t1 1score 2score t2))
+
+(defvar *leaguetable* nil)
+
+;; initializes table info with every team listed in *team-names*
+(defun init-table ()
+  (loop for team in *team-names*
+	do (push
+	    (list :name team :w 0 :d 0 :l 0 :gf 0 :ga 0 :pts 0)
+	    *leaguetable*)))
+
+;; will print a nice looking info for each team
+(defun table ()
+  (dolist (team *leaguetable*)
+    (format t "~{~a:~6t~a~%~}~%" team)))
+
+;; run this after adding your desired teams via add-team
+(defun setup ()
+  (init-names)
+  (init-table))
+
+
